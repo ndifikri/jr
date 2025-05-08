7f1d34307082dcd431dac3fa294a68afdfe4ba86
rusnandi.fikri <rusnandi.fikri@cpssoft.com>
2025-05-08 11:17:22 +0700

    improve summarize

--- summarize.py
+++ summarize.py
@@ -1,10 +1,44 @@
 import streamlit as st
-import base64
-
+import base64, time
+from streamlit_theme import st_theme
 from openai import OpenAI
 
+with st.spinner("Preparing Application", show_time=True):
+    theme_json = st_theme()
+    time.sleep(1)
+    theme = theme_json['base']
+
+def get_base64_of_image(image_path):
+    with open(image_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode()
+    
+
+# Custom CSS to set the background image
+def set_background_image(image_path):
+    encoded_image = get_base64_of_image(image_path)
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded_image}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+if theme == "dark":
+    background_image_path = "./dark_bg.png"
+else:
+    background_image_path = "./light_bg.png"
+
+set_background_image(background_image_path)
+
 # Judul aplikasi llm
-st.title("Deteksi Ekstensi File")
+st.title("Summarize Document")
 
 # User input for API key
 api_key = st.text_input("Enter your API Key:", type="password")
